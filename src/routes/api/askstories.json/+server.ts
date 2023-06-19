f416d404f18af4cd1ab547f7a8e77b8f57354662
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-06-19 08:49:27 -0700

    cleanup

--- src/routes/api/askstories.json/+server.ts
+++ src/routes/api/askstories.json/+server.ts
@@ -5,7 +5,6 @@ import { json } from '@sveltejs/kit'
 export const GET = async () => {
   const response = await fetch('https://hacker-news.firebaseio.com/v0/askstories.json');
   const ask_stories = await response.json()
-  console.log('topStories:', ask_stories);
 
   return json(ask_stories)
 }
