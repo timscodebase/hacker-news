439e2904c6f706bc041d3c2e1994dd3f9c28b416
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-06-19 07:15:41 -0700

    SEO

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,6 @@
+export const title = 'Hacker News Clone'
+export const description = 'Hacker News Clone built with React, Next.js, and TypeScript'
+
 export const base_url = 'http://localhost:5173'
 export const number_of_topstories = 30
 export const number_of_askstories = 30
