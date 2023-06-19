f459a9d77c86cff4ad8012900cbbe1330d559eae
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-06-19 08:18:05 -0700

    SEO

--- src/lib/components/ExternalLink.svelte
+++ src/lib/components/ExternalLink.svelte
@@ -7,3 +7,14 @@
 <a {rel} class={className} {href} data-sveltekit-preload-data="hover">
 	<slot />
 </a>
+
+<style>
+	/* Mobile Media Query */
+	@media (max-width: 800px) {
+		a {
+			display: inline-block;
+			margin: 0.25rem;
+			padding: 0.25rem;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import './app.css';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 </script>
