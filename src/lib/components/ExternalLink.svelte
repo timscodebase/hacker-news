1c5aeb4bfb158ce43aeb49035dc754e0945994fb
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-06-19 08:23:41 -0700

    SEO

--- src/lib/components/ExternalLink.svelte
+++ src/lib/components/ExternalLink.svelte
@@ -7,14 +7,3 @@
 <a {rel} class={className} {href} data-sveltekit-preload-data="hover">
 	<slot />
 </a>
-
-<style>
-	/* Mobile Media Query */
-	@media (max-width: 800px) {
-		a {
-			display: inline-block;
-			margin: 0.25rem;
-			padding: 0.25rem;
-		}
-	}
-</style>
